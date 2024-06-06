b5c2e87584c74d8c15a9fde70f248deb80b621d0
zhz0704 <eatcoc10@gmail.com>
2024-06-06 21:07:46 +0800

    feat: 用fine-tune後的chatGPT生成多個標題

--- fine_tuned_chatGPT_title_gen.py
+++ fine_tuned_chatGPT_title_gen.py
@@ -7,6 +7,8 @@ with open('api_key.txt', 'r') as file:
 with open('model_name.txt', 'r') as file:
     model_name = file.read()
 
+client = OpenAI(api_key=key)
+
 promptTypes = {
     "驚嘆": "驚嘆語句",
     "疑問": "疑問語句",
@@ -18,11 +20,14 @@ promptTypes = {
 outline_folder = 'testing_compare_outline/'
 title_folder = 'testing_compare_title/'
 videoDataInputPath = 'testing_compare.csv'
+title_count = 5
 
 with open(videoDataInputPath, "r", encoding="utf-8") as videoDataFile:
     videoData = list(csv.reader(videoDataFile))[1:]
 
 for channel, title, processedTitle, prompts, duration, videoId in videoData:
+    if videoId != 'evBXDZ1dB_E':
+        continue
     with open(os.path.join(outline_folder, videoId + '.txt'), 'r', encoding='utf-8') as file:
         outline = file.read()
 
@@ -31,20 +36,24 @@ for channel, title, processedTitle, prompts, duration, videoId in videoData:
     for prompt in prompts:
         prompt_str += promptTypes[prompt] + "、"
     prompt_str = prompt_str[:-1] + "。"
-    # print(f"#zh-tw 請根據我提供的影片內容摘要想一個吸睛的 YouTube 影片標題。{prompt_str}摘要：{outline}")
-    
-    client = OpenAI(api_key=key)
+    # print(f"#zh-tw 請根據以下的影片摘要想一個吸睛的 YouTube 影片標題。{prompt_str}\n摘要：{outline}")
+    # print('---')
+    # continue
+    titles = []
+    for i in range(title_count):
+        completion = client.chat.completions.create(
+            model=model_name,
+            messages = [
+                {"role":"system","content":"你是一位在 YouTube 平台上的影音創作者，你擅長撰寫吸引人的影片標題。"},
+                {"role":"user", "content": f"#zh-tw 請根據以下影片摘要想一個吸睛的 YouTube 影片標題。{prompt_str}\n摘要：{outline}"}
+            ]
+        )
 
-    completion = client.chat.completions.create(
-        model=model_name,
-        messages = [
-            {"role":"system","content":"你是一位在 YouTube 平台上的影音創作者，你擅長撰寫吸引人的影片標題。"},
-            {"role":"user", "content": f"#zh-tw 請根據以下影片摘要想一個吸睛的 YouTube 影片標題。{prompt_str}摘要：{outline}"}
-        ]
-    )
-
-    response = completion.choices[0].message.content
-    print(response)
+        response = completion.choices[0].message.content
+        if response[-1] == '\n':
+            response = response[:-1]
+        titles.append(response)
+        print(response)
 
     with open(os.path.join(title_folder, videoId + '.txt'), 'w', encoding='utf-8') as file:
-        file.write(response)
+        file.write('\n'.join(titles))
