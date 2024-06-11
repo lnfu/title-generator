4b734eec66b397d55abd7354c08a7accc5868253
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2024-06-11 18:40:11 +0800

    fix: 去除一個句子太多重複字的情況 (OpenAI 會 ban)
    如果 not isTextValid(text) 直接丟掉 text (句子)

--- generate.py
+++ generate.py
@@ -24,12 +24,10 @@ def parse_arguments():
     parser.add_argument(
         "-t",
         "--types",
-        choices=list(TITLE_TYPE_DESCRIPTIONS.keys()),
-        type=int,
-        default=5,
-        help="The title types you want",
+        type=str,
+        nargs="+",
+        help="The title types you want e.g., 驚嘆,疑問,項目數,數據,轉折",
     )
-    # TODO 選項是多選多，不是多選一
     parser.add_argument(
         "-o",
         "--output-directory",
@@ -87,7 +85,7 @@ if __name__ == "__main__":
     # 產生標題
     titles = generateTitle(
         args.number,
-        list(filter(None, args.types.split(","))),
+        list(filter(None, args.types.split(" "))),
         os.getenv("FINE_TUNED_MODEL_NAME"),
         outlineDirectory,
         videoId,
